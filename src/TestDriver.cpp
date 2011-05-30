98300b97f7479259f6eb8b4d3abb6ee18daef896
Joshua Downer <joshua.downer@gmail.com>
2011-05-30 09:50:49 -0400

    Implemented simple test driver.

--- src/TestDriver.cpp
+++ src/TestDriver.cpp
@@ -1,6 +1,28 @@
 #include "TestDriver.h"
+#include <iostream>
 
-void TestDriver::runTests()
+TestDriver::TestDriver() : m_tests(0)
 {
 }
 
+void TestDriver::runTests()
+{
+  typedef std::vector<std::pair<std::string,TestFunction> > Tests;
+
+  std::cout << "Starting tests" << std::endl;
+
+  for(Tests::const_iterator it = m_tests.begin(); it != m_tests.end(); ++it)
+  {
+    if (not it->second())
+    {
+      std::cout << "Failed: " << it->first << std::endl;
+    }
+  }
+
+  std::cout << "Tests complete" << std::endl;
+}
+
+void TestDriver::registerTest(const std::string& name, TestFunction test)
+{
+  m_tests.push_back(std::make_pair(name, test));
+}
--- src/TestDriver.h
+++ src/TestDriver.h
@@ -1,13 +1,24 @@
 #ifndef TESTDRIVER_H
 #define TESTDRIVER_H
 
+#include <vector>
+#include <string>
+
+typedef bool(*TestFunction)(void);
+
 /**
  *
  */
 class TestDriver
 {
   public:
+    TestDriver();
+
     void runTests();
+    void registerTest(const std::string& name, TestFunction test);
+
+  private:
+    std::vector<std::pair<std::string, TestFunction> > m_tests;
 };
 
 #endif // TESTDRIVER_H
