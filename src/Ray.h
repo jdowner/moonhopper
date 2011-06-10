2400f7922c2d0fb6aa33ee23161e9635fcea4b01
Joshua Downer <jdowner@heartlandrobotics.com>
2011-06-09 20:25:17 -0400

    Added missing file

---
+++ src/Ray.h
@@ -0,0 +1,13 @@
+#ifndef RAY_H
+#define RAY_H
+
+struct Ray
+{
+  double ox; // origin of the ray (x co-ordinate)
+  double oy; // origin of the ray (y co-ordinate)
+  double nx; // direction of the ray (x co-ordinate)
+  double ny; // direction of the ray (y co-ordinate)
+};
+
+#endif // RAY_H
+
