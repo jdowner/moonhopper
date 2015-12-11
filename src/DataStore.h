7319686fe078d54aff52884fd87675b227ddc928
Joshua Downer <joshua.downer@gmail.com>
2015-12-11 07:19:02 -0500

    wip

--- src/DataStore.h
+++ src/DataStore.h
@@ -2,11 +2,60 @@
 #define DATASTORE_H
 
 #include <string>
+#include <vector>
 #include <sstream>
+#include <fstream>
 #include <stdexcept>
 #include <map>
 #include <cassert>
 #include <boost/lexical_cast.hpp>
+
+class DataValue
+{
+  public:
+    template <typename T>
+    T as() const {
+      T var;
+      std::istringstream iss;
+      iss.str(m_value);
+      iss >> var;
+      return var;
+    }
+
+    const std::string& get() const noexcept {
+      return m_value;
+    }
+
+  private:
+    std::string m_value;
+};
+
+class DataContainer : public DataValue
+{
+  public:
+    typedef std::vector<DataValue>::iterator iterator;
+    typedef std::vector<DataValue>::const_iterator const_iterator;
+
+  public:
+    const DataValue& append(const DataValue& value){
+      m_values.push_back(value);
+    }
+
+    iterator begin() { return m_values.begin(); }
+    const_iterator begin() const { return m_values.begin(); }
+
+    iterator end() { return m_values.end(); }
+    const_iterator end() const { return m_values.end(); }
+
+  private:
+    std::vector<DataValue> m_values;
+};
+
+class KVStore
+{
+  private:
+    std::map<std::string, DataValue> m_values;
+};
 
 /**
  *
